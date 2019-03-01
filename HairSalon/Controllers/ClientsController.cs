6fb0c4245bda664aa7e3688baf68cf394832fa71
Mark Strickland <mstrickland@globys.com>
2019-03-01 14:09:13 -0800

    Add the Client Index route and view file.

---
+++ HairSalon/Controllers/ClientsController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+using System.Collections.Generic;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    [HttpGet("/clients")]
+    public ActionResult Index()
+    {
+      List<Client> allClients = Client.GetAll();
+      return View(allclients);
+    }
+
+
+  }
+}
