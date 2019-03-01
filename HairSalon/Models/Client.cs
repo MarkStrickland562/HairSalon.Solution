c3c614115bddab95e4381ba1a9de633ab4776354
Mark Strickland <mstrickland@globys.com>
2019-03-01 10:49:06 -0800

    Add ClearAll() method to Stylist and Client classes.

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -19,5 +19,49 @@ namespace HairSalon.Models
       _id = id;
     }
 
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public string GetGender()
+    {
+      return _gender;
+    }
+
+    public void SetGender(string newGender)
+    {
+      _gender = newGender;
+    }
+
+    public int GetStylistId()
+    {
+      return _stylistId;
+    }
+
+    public void SetStylistId(int newStylistId)
+    {
+      _stylistId = newStylistId;
+    }
+
+    public static void ClearAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM stylists;";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
   }
 }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -19,5 +19,49 @@ namespace HairSalon.Models
       _id = id;
     }
 
+    public string GetName()
+    {
+      return _name;
+    }
+
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+
+    public string GetSpecialty()
+    {
+      return _specialty;
+    }
+
+    public void SetSpecialty(string newSpecialty)
+    {
+      _specialty = newSpecialty;
+    }
+
+    public DateTime GetHireDate()
+    {
+      return _hireDate;
+    }
+
+    public void SetHireDate(DateTime newHireDate)
+    {
+      _hireDate = newHireDate;
+    }
+
+    public static void ClearAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM clients;";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
   }
 }
