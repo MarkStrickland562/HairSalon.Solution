d8c722aa5f8d25a82632e66eaec192da4f3ec41f
Mark Strickland <mstrickland@globys.com>
2019-03-08 10:17:32 -0800

    Add Specialty model

--- HairSalon/Models/Specialty.cs
+++ HairSalon/Models/Specialty.cs
@@ -4,15 +4,128 @@ using MySql.Data.MySqlClient;
 
 namespace HairSalon.Models
 {
+  public class Specialty
+  {
+    private string _specialty;
+    private int _id;
 
-// public string GetSpecialty()
-// {
-//   return _specialty;
-// }
-//
-// public void SetSpecialty(string newSpecialty)
-// {
-//   _specialty = newSpecialty;
-// }
+    public Specialty(string specialty, int id = 0)
+    {
+      _specialty = specialty;
+      _id = id;
+    }
 
+    public string GetSpecialty()
+    {
+      return _specialty;
+    }
+
+    public void SetSpecialty(string newSpecialty)
+    {
+      _specialty = newSpecialty;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+
+    public static void ClearAll()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM specialties;";
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public void Save()
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"INSERT INTO specialties (specialty) VALUES (@specialty);";
+      MySqlParameter specialty = new MySqlParameter();
+      specialty.ParameterName = "@specialty";
+      specialty.Value = this._specialty;
+      cmd.Parameters.Add(specialty);
+      cmd.ExecuteNonQuery();
+      _id = (int) cmd.LastInsertedId;
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
+    public static List<Specialty> GetAll()
+    {
+      List<Specialty> allSpecialties = new List<Specialty> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM specialties;";
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int SpecialtyId = rdr.GetInt32(0);
+        string Specialty = rdr.GetString(1);
+        Specialty newSpecialty = new Specialty(Specialty, SpecialtyId);
+        allSpecialties.Add(newSpecialty);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return allSpecialties;
+    }
+
+    public static Specialty Find(int id)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM specialties WHERE id = (@searchId);";
+      MySqlParameter searchId = new MySqlParameter();
+      searchId.ParameterName = "@searchId";
+      searchId.Value = id;
+      cmd.Parameters.Add(searchId);
+      var rdr = cmd.ExecuteReader() as MySqlDataReader;
+      int SpecialtyId = 0;
+      string Specialty = "";
+      while(rdr.Read())
+      {
+        SpecialtyId = rdr.GetInt32(0);
+        Specialty = rdr.GetString(1);
+      }
+      Specialty newSpecialty = new Specialty(Specialty);
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+      return newSpecialty;
+    }
+
+    public override bool Equals(System.Object otherSpecialty)
+    {
+      if (!(otherSpecialty is Specialty))
+      {
+        return false;
+      }
+      else
+      {
+        Specialty newSpecialty = (Specialty) otherSpecialty;
+        bool idEquality = this.GetId().Equals(newSpecialty.GetId());
+        bool specialtyEquality = this.GetSpecialty().Equals(newSpecialty.GetSpecialty());
+        return (idEquality && specialtyEquality);
+      }
+    }
+  }
 }
